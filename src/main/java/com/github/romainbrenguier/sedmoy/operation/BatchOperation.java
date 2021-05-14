3c998d4a860552955a8170fd2c476a6358b70f1e
Romain Brenguier <romain.brenguier@gmail.com>
2021-05-14 16:54:59 +0100

    Refactor Operations to allow sorting to be an operation

---
+++ src/main/java/com/github/romainbrenguier/sedmoy/operation/BatchOperation.java
@@ -0,0 +1,45 @@
+package com.github.romainbrenguier.sedmoy.operation;
+
+import com.github.romainbrenguier.sedmoy.operation.Operation.State;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+/** Operation which applies to a stream of objects rather than a single one */
+public interface BatchOperation {
+
+  List<State> apply(List<Operation.State> states);
+
+  static Object transformArrayToList(Object input) {
+    if (input.getClass().isArray()) {
+      return Arrays.stream((Object[])input).collect(Collectors.toList());
+    }
+    return input;
+  }
+
+  class Objectwise implements BatchOperation {
+    private final Operation underlying;
+
+    public Objectwise(Operation underlying) {
+      this.underlying = underlying;
+    }
+
+    @Override
+    public List<State> apply(List<State> states) {
+      Stack<Object> stack = states.get(states.size() - 1).stack;
+      for (int i = 0; i < states.size(); ++i) {
+        Operation.State state = new Operation.State(stack, states.get(i).data);
+        try {
+          state = underlying.apply(state);
+          state.data = transformArrayToList(state.data);
+        } catch (Exception e) {
+          state.data = e.getMessage();
+        }
+        stack = state.stack;
+        states.set(i, state);
+      }
+      return states;
+    }
+  }
+}
--- src/main/java/com/github/romainbrenguier/sedmoy/operation/Operations.java
+++ src/main/java/com/github/romainbrenguier/sedmoy/operation/Operations.java
@@ -1,8 +1,7 @@
 package com.github.romainbrenguier.sedmoy.operation;
 
-import com.github.romainbrenguier.sedmoy.operation.Operation.State;
+import com.github.romainbrenguier.sedmoy.operation.BatchOperation.Objectwise;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 import java.util.stream.Collectors;
@@ -11,40 +10,22 @@ import java.util.stream.Collectors;
  * Wrapper for a list of operations.
  */
 public class Operations {
-  private final List<Operation> operationList = new ArrayList<>();
+  private final List<BatchOperation> operationList = new ArrayList<>();
 
   public void removeLast() {
       operationList.remove(operationList.size() - 1);
   }
 
   public void add(Operation operation) {
-      operationList.add(operation);
-  }
-
-  private static Object transformArrayToList(Object input) {
-    if (input.getClass().isArray()) {
-      return Arrays.stream((Object[])input).collect(Collectors.toList());
-    }
-    return input;
+      operationList.add(new Objectwise(operation));
   }
 
   public List<Operation.State> apply(List<Object> inputData) {
-    Stack<Object> stack = new Stack<>();
     List<Operation.State> states = inputData.stream()
         .map(data -> new Operation.State(new Stack<>(), data))
         .collect(Collectors.toList());
-    for (final Operation operation : operationList) {
-      for (int i = 0; i < states.size(); ++i) {
-        Operation.State state = new Operation.State(stack, states.get(i).data);
-        try {
-          state = operation.apply(state);
-          state.data = transformArrayToList(state.data);
-        } catch (Exception e) {
-          state.data = e.getMessage();
-        }
-        stack = state.stack;
-        states.set(i, state);
-      }
+    for (final BatchOperation operation : operationList) {
+      states = operation.apply(states);
     }
     return states;
   }
