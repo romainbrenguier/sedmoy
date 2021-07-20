5c0376057a2e489fc1255ec267eadf8197352eb8
Romain Brenguier <romain.brenguier@gmail.com>
2021-07-20 11:24:01 +0100

    Simple UI

--- src/ocaml/main.ml
+++ src/ocaml/main.ml
@@ -1,1 +1,12 @@
-let _ = print_endline "Hello!"
+open GMain
+
+let locale = GtkMain.Main.init ()
+
+let _ =
+  (* Simple gtk example extracted from https://ocaml.org/learn/tutorials/introduction_to_gtk.html *)
+  let window = GWindow.window ~width:320 ~height:240
+                              ~title:"Simple lablgtk program" () in
+  let vbox = GPack.vbox ~packing:window#add () in 
+  ignore vbox;
+  window#show ();
+  Main.main ()
