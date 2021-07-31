ff09dc3640e514dee9b15287a1859b6f41569e01
Romain Brenguier <romain.brenguier@gmail.com>
2021-07-31 21:29:12 +0100

    Implement more operations

--- src/ocaml/main.ml
+++ src/ocaml/main.ml
@@ -5,17 +5,24 @@ let locale = GtkMain.Main.init ()
 let join ~separator = function 
   | [] -> "" | head :: tail -> List.fold_left (fun a b -> a ^ separator ^ b) head tail
 
+let rec limit accu size list = match size, list with
+  | 0, _ | _, [] -> List.rev accu 
+  | s, head :: tail -> limit (head :: accu) (s - 1) tail
+
 let evaluate command_text input = 
-  let command = Operation.parse command_text in
-  String.split_on_char '\n' input
-  |> List.map (fun x -> Operation.String x)
-  |> Operation.evaluate command
-  |> List.map (fun x -> Operation.value_to_string x)
+  let lines = String.split_on_char '\n' command_text in
+  let commands = List.map Operation.parse lines in
+  let input_values = String.split_on_char '\n' input |> limit [] 7 |> List.map (fun x -> Operation.String x) in
+  let output_values = List.map (fun command -> Operation.evaluate command input_values) commands in
+  List.map 
+    (fun output -> List.map (fun x -> Operation.value_to_string x) output |> join ~separator:"\n") 
+    output_values
+  |> join ~separator:"\n===================\n"
     
 let run_ui input_list = 
   (* Simple gtk example extracted from https://ocaml.org/learn/tutorials/introduction_to_gtk.html *)
   let window = GWindow.window
-    ~width:800 ~height:600
+    ~width:1200 ~height:800
     ~title:"Simple lablgtk program" () in
     
   let vbox = GPack.vbox ~packing:window#add () in 
@@ -26,19 +33,17 @@ let run_ui input_list =
                 ~hpolicy:`AUTOMATIC ~vpolicy:`AUTOMATIC
                 ~packing:vbox#pack () in
 *)
-  let text_edit = GText.view ~height:300 ~editable:true ~packing:vbox#pack (*scroll#add_with_viewport*) () in
-  text_edit#buffer#set_text "multi-\nline\ntext";
-  
   let button = GButton.button ~label:"Evaluate" ~packing:vbox#add () in
   let hbox = GPack.hbox ~packing:vbox#add () in
-  let input_text_view = GText.view ~height:250 ~width:400 ~editable:false ~packing:hbox#pack () in
-  let output_text_view = GText.view ~height:250 ~width:400 ~editable:false ~packing:hbox#pack () in
+  let input_text_view = GText.view  ~height:700 ~width:400 ~editable:false ~packing:hbox#pack () in
+  let text_edit = GText.view ~height:700 ~width:400 ~border_width:5 ~editable:true ~packing:hbox#pack (*scroll#add_with_viewport*) () in
+  let output_text_view = GText.view  ~height:700 ~width:400 ~editable:false ~packing:hbox#pack () in
+  text_edit#buffer#set_text "";
   input_text_view#buffer#set_text (join ~separator:"\n" input_list);
   output_text_view#buffer#set_text "Output text";
   button#connect#clicked 
     ~callback:(fun () -> 
       evaluate (text_edit#buffer#get_text()) (input_text_view#buffer#get_text())
-      |> join ~separator:"\n"
       |> output_text_view#buffer#set_text)
    |> ignore ;
 
--- src/ocaml/operation.ml
+++ src/ocaml/operation.ml
@@ -1,9 +1,13 @@
 type argument_type = StringT | NumberT
+type value_type = StringValue | NumberValue | ListValue of value_type
 type argument_value = StringArg of string | NumberArg of int
 type value = String of string | Number of int
-type signature = { name: string; argument_types: argument_type list; 
-    implementation: argument_value list -> value -> value}
-type t = {signature: signature; argument_values: argument_value list}
+type signature = { 
+  name: string; argument_types: argument_type list;
+  input_type: value_type; output_type: value_type; 
+  implementation: argument_value list -> value -> value}
+type t = {
+  signature: signature; argument_values: argument_value list;}
 
 let value_to_string = function String s -> s | Number i -> string_of_int i
 
@@ -11,8 +15,34 @@ let substring arguments value = match arguments, value with
   | NumberArg i :: [], String s -> String (String.sub s 0 i)
   | _ -> failwith "unreachable" 
 
-let signatures = 
-  [{name="substring"; argument_types=[NumberT]; implementation=substring}]
+let concat arguments value = match arguments, value with
+  | StringArg arg :: [], String s -> String (s ^ arg)
+  | _ -> failwith "unreachable" 
+
+let index_of arguments value = match arguments, value with
+  | StringArg s :: [], String t -> Number (try Str.search_forward (Str.regexp s) t 0 with Not_found -> -1)
+  | _ -> failwith "unreachable" 
+
+let to_string arguments value = match arguments, value with
+  | [], Number n -> String (string_of_int n)
+  | _ -> failwith "unreachable" 
+
+let length arguments value = match arguments, value with
+  | [], String s -> Number (String.length s)
+  | _ -> failwith "unreachable" 
+
+let replace_all arguments value = match arguments, value with
+  | StringArg replace :: StringArg by :: [], String s -> String (Str.global_replace (Str.regexp replace) by s)
+  | _ -> failwith "unreachable" 
+
+let signatures = [
+  {name="concat"; argument_types=[StringT]; implementation=concat; input_type=StringValue; output_type=StringValue};
+  {name="index_of"; argument_types=[StringT]; implementation=index_of; input_type=StringValue; output_type=NumberValue};
+  {name="substring"; argument_types=[NumberT]; implementation=substring; input_type=StringValue; output_type=StringValue};
+  {name="to_string"; argument_types=[]; implementation=to_string; input_type=NumberValue; output_type=StringValue};
+  {name="length"; argument_types=[]; implementation=length; input_type=StringValue; output_type=NumberValue};
+  {name="replace_all"; argument_types=[StringT; StringT]; implementation=replace_all; input_type=StringValue; output_type=StringValue};
+  ]
 
 let rec parse_arguments accu = function
   | [], lexems -> List.rev accu, lexems 
@@ -23,7 +53,7 @@ let rec parse_arguments accu = function
   | _ -> failwith "invalid arguments"
 
 let rec parse_lexems accu = function
-  | [] -> accu
+  | [] -> List.rev accu
   | head :: tail -> 
     let signature = List.find (fun x -> x.name = head) signatures in
     let argument_values, remainder = parse_arguments [] (signature.argument_types, tail) in 
