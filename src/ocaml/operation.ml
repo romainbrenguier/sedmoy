fc87b87154d759cd17c04a94889d753488ca65bb
Romain Brenguier <romain.brenguier@gmail.com>
2021-08-19 21:25:25 +0100

    Add more operations

--- src/ocaml/operation.ml
+++ src/ocaml/operation.ml
@@ -23,6 +23,10 @@ let concat arguments value = match arguments, value with
   | StringArg arg :: [], StringValue s -> StringValue (s ^ arg)
   | _ -> failwith "unreachable" 
 
+let prefix_with arguments value = match arguments, value with
+  | StringArg arg :: [], StringValue s -> StringValue (arg ^ s)
+  | _ -> failwith "unreachable" 
+
 let index_of arguments value = match arguments, value with
   | StringArg s :: [], StringValue t -> NumberValue (try Str.search_forward (Str.regexp s) t 0 with Not_found -> -1)
   | _ -> failwith "unreachable" 
@@ -44,6 +48,11 @@ let split arguments value = match arguments, value with
     ListValue (String.split_on_char separator.[0] s |> List.map (fun s -> StringValue s))
   | _ -> failwith "unreachable" 
 
+let join arguments value = match arguments, value with
+  | StringArg separator :: [], ListValue l -> 
+    l |> List.map (function StringValue s -> s | _ -> failwith "unreachable") |> Util.join ~separator |> (fun x -> StringValue x)
+  | _ -> failwith "unreachable" 
+
 let signatures = [
   {name="concat"; argument_types=[StringArgType]; implementation=concat; input_type=StringType; output_type=StringType};
   {name="index_of"; argument_types=[StringArgType]; implementation=index_of; input_type=StringType; output_type=NumberType};
@@ -52,6 +61,8 @@ let signatures = [
   {name="length"; argument_types=[]; implementation=length; input_type=StringType; output_type=NumberType};
   {name="replace_all"; argument_types=[StringArgType; StringArgType]; implementation=replace_all; input_type=StringType; output_type=StringType};
   {name="split"; argument_types=[StringArgType]; implementation=split; input_type=StringType; output_type= ListType StringType};
+  {name="join"; argument_types=[StringArgType]; implementation=join; input_type=ListType StringType; output_type= StringType};
+  {name="prefix_with"; argument_types=[StringArgType]; implementation=prefix_with; input_type=StringType; output_type= StringType};
   ]
 
 let rec parse_arguments accu = function
@@ -84,3 +95,4 @@ let starts_with prefix string =
 
 let suggest_command prefix =
   List.filter (fun x -> starts_with prefix x.name) signatures
+  
