d57788d58302d1a9153bfdb2d280d3fe7e743f30
Romain Brenguier <romain.brenguier@gmail.com>
2021-08-21 18:17:06 +0100

    Auto evaluate on text input

--- src/ocaml/main.ml
+++ src/ocaml/main.ml
@@ -21,11 +21,14 @@ let evaluate command_text input =
 let save_as_html command_text input =
   let input_values = String.split_on_char '\n' input |> List.map (fun x -> Operation.StringValue x) in
   let result = evaluate_values command_text input_values in
-  let out_channel = open_out "/tmp/sedmoy.out" in
+  let out_channel = open_out "/tmp/sedmoy.html" in
   Printf.fprintf out_channel "<html><head></head><body>\n";
   List.iter (fun line -> Printf.fprintf out_channel "<p>%s\n" line) result; 
   Printf.fprintf out_channel "</body></html>\n";
   close_out out_channel
+
+let save_as_mobi html_file =
+  "ebook-convert", html_file, "/tmp/sedmoy.mobi"
 
 let suggest_text prefix = 
   Operation.suggest_command prefix |> List.map (fun x -> x.Operation.name) |> Util.join ~separator:"   "
@@ -48,11 +51,12 @@ let run_ui input_list =
   let button = GButton.button ~label:"Evaluate" ~packing:buttons_hbox#add () in
   let button_save_html = GButton.button ~label:"Save as html" ~packing:buttons_hbox#add () in
 
-  let suggestions = GText.view ~height:50 ~editable:false ~packing:vbox#add () in
+  let suggestions = GText.view ~height:50 ~editable:false ~border_width:1 ~packing:vbox#add () in
   let hbox = GPack.hbox ~packing:vbox#add () in
-  let input_text_view = GText.view  ~height:700 ~width:400 ~editable:false ~packing:hbox#pack () in
-  let text_edit = GText.view ~height:700 ~width:400 ~border_width:5 ~editable:true ~packing:hbox#pack (*scroll#add_with_viewport*) () in
-  let output_text_view = GText.view  ~height:700 ~width:400 ~editable:false ~packing:hbox#pack () in
+  let input_text_view = GText.view  ~height:700 ~width:400 ~border_width:1 ~editable:false ~packing:hbox#pack () in
+  let text_edit = GText.view ~height:700 ~width:400 ~border_width:1 ~editable:true ~packing:hbox#pack (*scroll#add_with_viewport*) () in
+  (* TODO: add new text zone for global commands *)
+  let output_text_view = GText.view  ~height:700 ~width:400 ~border_width:1 ~editable:false ~packing:hbox#pack () in
   suggestions#buffer#set_text (suggest_text "");
   text_edit#buffer#set_text "";
   input_text_view#buffer#set_text (Util.join ~separator:"\n" input_list);
@@ -71,7 +75,9 @@ let run_ui input_list =
 
   text_edit#buffer#connect#changed ~callback:(fun () -> 
     let current = Util.current_word (text_edit#buffer#get_text()) (text_edit#buffer#cursor_position) in
-    suggestions#buffer#set_text (suggest_text current) 
+    suggestions#buffer#set_text (suggest_text current) |> ignore;
+    evaluate (text_edit#buffer#get_text()) (input_text_view#buffer#get_text())
+      |> output_text_view#buffer#set_text
   ) |> ignore;
 
   window#show ();
