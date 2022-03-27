a85be78bc1d7605b75c9c32a0394d14478d3cdda
Romain Brenguier <romain.brenguier@gmail.com>
2022-03-27 17:55:36 +0100

    Use picocli for commandline arguments

--- src/main/java/com/github/romainbrenguier/sedmoy/Main.java
+++ src/main/java/com/github/romainbrenguier/sedmoy/Main.java
@@ -3,21 +3,25 @@ package com.github.romainbrenguier.sedmoy;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-public class Main {
+@Command
+public class Main implements Runnable {
+  @Option(names = "--path")
+  Path path;
+
   public static void main(String[] args) {
-    Path path = Paths.get(args[0]);
-    MainConfig config = new MainConfig().withPath(path);
-    run(config);
+    CommandLine.run(new Main(), args);
   }
 
-  public static void run(MainConfig config) {
-    assert config.isInteractive() : "only interactive mode is supported";
+  @Override
+  public void run() {
     try {
       final Preferences preferences = Preferences.load();
-      final List<String> data = Files.readAllLines(config.path);
+      final List<String> data = Files.readAllLines(path);
       new InteractiveMode(data, preferences).run();
       preferences.save();
     } catch (IOException e) {
