bf99a2ac236852bdc76e5b7b03ef95b5963f5460
Romain Brenguier <romain.brenguier@gmail.com>
2022-03-27 18:18:43 +0100

    Make groovy interpreter callable from main

--- src/main/java/com/github/romainbrenguier/sedmoy/GroovyInterpreter.java
+++ src/main/java/com/github/romainbrenguier/sedmoy/GroovyInterpreter.java
@@ -3,12 +3,32 @@ package com.github.romainbrenguier.sedmoy;
 
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
+import java.io.File;
+import java.io.IOException;
 
 public class GroovyInterpreter {
+  static class GroovyException extends Exception {
+    final private File groovyFile;
 
-  public static void main(String[] args) {
+    GroovyException(File groovyFile, Exception cause) {
+      super(cause);
+      this.groovyFile = groovyFile;
+    }
+
+    @Override
+    public String getMessage() {
+      return "Error while reading script: " + groovyFile;
+    }
+  }
+
+  public static void run(File groovyScript) throws GroovyException {
     final Binding binding = new Binding();
     final GroovyShell shell = new GroovyShell(binding);
-    shell.evaluate("println 'hello'");
+    try {
+      final Object result = shell.evaluate(groovyScript);
+      System.out.println(result.toString());
+    } catch (IOException exception) {
+      throw new GroovyException(groovyScript, exception);
+    }
   }
 }
--- src/main/java/com/github/romainbrenguier/sedmoy/Main.java
+++ src/main/java/com/github/romainbrenguier/sedmoy/Main.java
@@ -1,5 +1,6 @@
 package com.github.romainbrenguier.sedmoy;
 
+import com.github.romainbrenguier.sedmoy.GroovyInterpreter.GroovyException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,12 +14,23 @@ public class Main implements Runnable {
   @Option(names = {"--input", "-i"}, required = true)
   Path input;
 
+  @Option(names = {"--groovy-script", "-g"})
+  Path groovyScript;
+
   public static void main(String[] args) {
     CommandLine.run(new Main(), args);
   }
 
   @Override
   public void run() {
+    if (groovyScript != null) {
+      try {
+        GroovyInterpreter.run(groovyScript.toFile());
+      } catch (GroovyException e) {
+        e.printStackTrace();
+      }
+      return;
+    }
     try {
       final Preferences preferences = Preferences.load();
       final List<String> data = Files.readAllLines(input);
