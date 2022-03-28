5819153bbb32854e14b12e76ee0d0a5bc7a373b6
Romain Brenguier <romain.brenguier@gmail.com>
2022-03-28 22:43:19 +0100

    Make a script a bit more complete

--- scripts/example.groovy
+++ scripts/example.groovy
@@ -1,3 +1,19 @@
+import com.github.romainbrenguier.sedmoy.csv.Table
 
-println "Input first 10 lines:\n${input.limit(10)}"
+def table = input as Table
 
+println "Input first 10 lines:\n${table.upFrom(10)}"
+
+def keySet = [] as HashSet<String>
+
+for (int i = 0; i < table.width(); ++i) {
+    println("$i : ${table.cell(i, 0)}")
+}
+
+for (int i = 0; i < table.height(); ++i) {
+    def cell = table.cell(1, i)
+    if (cell.size() >= 2)
+        keySet.add(cell.substring(0, 2))
+}
+
+println keySet
