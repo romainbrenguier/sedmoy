e2fffe06ad02907e7d8780544784551266944af9
Romain Brenguier <romain.brenguier@gmail.com>
2022-04-11 20:29:12 +0100

    Improved example

---
+++ scripts/crop.groovy
@@ -0,0 +1,5 @@
+import com.github.romainbrenguier.sedmoy.csv.Table
+
+def table = input as Table
+
+println(table.upFrom(99))
--- scripts/example.groovy
+++ scripts/example.groovy
@@ -2,18 +2,38 @@ import com.github.romainbrenguier.sedmoy.csv.Table
 
 def table = input as Table
 
-println "Input first 10 lines:\n${table.upFrom(10)}"
+//table = table.upFrom(20)
 
-def keySet = [] as HashSet<String>
-
-for (int i = 0; i < table.width(); ++i) {
-    println("$i : ${table.cell(i, 0)}")
+static String normalize(String s) {
+    s.toLowerCase()
 }
+
+static String keyOfString(String s) {
+    normalize(s.size() > 2 ? s.substring(0, 2) : s)
+}
+
+def indexedTable = [:]
+def scores = [:]
+
+static int scoreOfRank(int rank) { 10000 / rank }
 
 for (int i = 0; i < table.height(); ++i) {
-    def cell = table.cell(1, i)
-    if (cell.size() >= 2)
-        keySet.add(cell.substring(0, 2))
+    def key = table.cell(1, i)
+    def value = table.cell(2, i)
+    indexedTable[key] = value
+    scores[keyOfString(key)] = scores.getOrDefault(keyOfString(key), 0) +
+            scoreOfRank(Integer.parseInt(table.cell(0, i)))
 }
 
-println keySet
+def grouped = indexedTable.groupBy { keyOfString it.key as String }
+
+static String formatSection(Map<String, String> map) {
+    map.collect { "$it.key : $it.value" }.join("\n  * ")
+}
+
+grouped.sort { entry -> -scores[entry.key] }
+        .collect {
+            entry ->
+                "# $entry.key [${scores[entry.key]}]\n\n" +
+                        "  * ${formatSection(entry.value as Map<String, String>)}"
+        }.join("\n\n")
