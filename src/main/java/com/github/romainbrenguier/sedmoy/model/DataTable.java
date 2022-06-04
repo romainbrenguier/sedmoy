a0bdeeca8ae7511e5a7df4c435de8f266ab0e3c3
Romain Brenguier <romain.brenguier@gmail.com>
2022-06-04 13:34:41 +0100

    Add setters to DataTable to make creation easier

--- src/main/java/com/github/romainbrenguier/sedmoy/model/DataTable.java
+++ src/main/java/com/github/romainbrenguier/sedmoy/model/DataTable.java
@@ -2,21 +2,29 @@ package com.github.romainbrenguier.sedmoy.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DataTable implements Table {
 
   private final List<Row> lines;
 
-  private final int width;
-
-  private final int height;
+  private final Dimension dimension;
 
   public DataTable(List<Row> lines) {
     this.lines = lines;
-    height = lines.size();
-    width = lines.stream().map(ArrayList::size).reduce(0, Math::max);
+    dimension = new Dimension(
+        lines.size(),
+        lines.stream().map(ArrayList::size).reduce(0, Math::max));
+  }
+
+  public DataTable(Dimension dimension) {
+    this.dimension = dimension;
+    this.lines = new ArrayList<>();
+    for (int line = 0; line < dimension.numberOfLines; ++line) {
+      lines.add(new Row(IntStream.range(0, dimension.numberOfColumns).mapToObj(i -> "")
+          .collect(Collectors.toList())));
+    }
   }
 
   public List<Row> getLines() {
@@ -24,11 +32,15 @@ public class DataTable implements Table {
   }
 
   public int height() {
-    return height;
+    return dimension.numberOfLines;
   }
 
   public int width() {
-    return width;
+    return dimension.numberOfColumns;
+  }
+
+  public void set(int columnIndex, int lineIndex, String content) {
+    lines.get(lineIndex).set(columnIndex, content);
   }
 
   public String cell(int columnIndex, int lineIndex) {
@@ -65,7 +77,7 @@ public class DataTable implements Table {
   }
 
   public DataTable upFrom(int lineIndex) {
-    return upFrom(width - 1, lineIndex);
+    return upFrom(width() - 1, lineIndex);
   }
 
   public DataTable downFrom(int lineIndex) {
@@ -73,7 +85,7 @@ public class DataTable implements Table {
   }
 
   public DataTable leftFrom(int columnIndex) {
-    return upFrom(columnIndex, height - 1);
+    return upFrom(columnIndex, height() - 1);
   }
 
   public DataTable rightFrom(int columnIndex) {
--- src/test/java/com/github/romainbrenguier/sedmoy/app/FormulaTableEvaluatorTest.java
+++ src/test/java/com/github/romainbrenguier/sedmoy/app/FormulaTableEvaluatorTest.java
@@ -2,13 +2,14 @@ package com.github.romainbrenguier.sedmoy.app;
 
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.github.romainbrenguier.sedmoy.model.DataTable;
 import com.github.romainbrenguier.sedmoy.model.Dimension;
 import com.github.romainbrenguier.sedmoy.model.FormulaTable;
-import com.github.romainbrenguier.sedmoy.model.Row;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,7 +17,35 @@ import org.junit.jupiter.api.Test;
 
 class FormulaTableEvaluatorTest {
 
-  /** Fake interpreter that returns the result of a hardcoded script */
+  @Test
+  void testEvaluate() {
+    GroovyInterpreter interpreter = new MockInterpreter();
+    final DataTable dataTable = new DataTable(new Dimension(2, 2));
+    dataTable.set(0, 0, "a");
+    dataTable.set(0, 1, "A");
+    dataTable.set(1, 0, "b");
+    final Map<String, DataTable> environment =
+        Collections.singletonMap("foo", dataTable);
+    final FormulaTable formulaTable = new FormulaTable(new Dimension(2, 2));
+
+    // Act
+    final DataTable result = new FormulaTableEvaluator()
+        .evaluate(interpreter, environment, formulaTable);
+
+    // Assert
+    assertThat(result, not(nullValue()));
+    assertEquals(2, result.width());
+    assertEquals(2, result.height());
+    // FIXME order of line and column differs which is confusing
+    assertThat(result.cell(0, 0), equalTo("foo[0,0]=a"));
+    assertThat(result.cell(0, 1), equalTo("foo[1,0]=b"));
+    assertThat(result.cell(1, 0), equalTo("foo[0,1]=A"));
+    assertThat(result.cell(1, 1), equalTo("foo[1,1]="));
+  }
+
+  /**
+   * Fake interpreter that returns the result of a hardcoded script
+   */
   class MockInterpreter extends GroovyInterpreter {
 
     Map<String, Object> variables = new HashMap<>();
@@ -28,25 +57,10 @@ class FormulaTableEvaluatorTest {
 
     @Override
     public Object run(String groovyScript) {
-      final Object i = variables.get("line");
-      final Object j = variables.get("column");
+      final int i = (int) variables.get("line");
+      final int j = (int) variables.get("column");
       final DataTable foo = (DataTable) variables.get("foo");
-      return foo.cell(0, 0) + ";" + i + ";" + j;
+      return "foo[" + i + "," + j + "]=" + foo.cell(i, j);
     }
-  }
-
-  @Test
-  void testEvaluate() {
-    GroovyInterpreter interpreter = new MockInterpreter();
-    final Map<String, DataTable> environment = Collections.singletonMap("foo",
-        new DataTable(Collections.singletonList(new Row(Collections.singletonList("bar")))));
-    final FormulaTable formulaTable = new FormulaTable(new Dimension(2, 2));
-
-    // Act
-    final DataTable result = new FormulaTableEvaluator()
-        .evaluate(interpreter, environment, formulaTable);
-
-    // Assert
-    assertThat(result, not(nullValue()));
   }
 }
