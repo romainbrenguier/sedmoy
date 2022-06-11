482dcd6245001612d2c063caa786ddb6ad1df3e8
Romain Brenguier <romain.brenguier@gmail.com>
2022-06-11 22:24:41 +0100

    Refactor evaluate

--- src/main/java/com/github/romainbrenguier/sedmoy/app/FormulaTableEvaluator.java
+++ src/main/java/com/github/romainbrenguier/sedmoy/app/FormulaTableEvaluator.java
@@ -2,8 +2,6 @@ package com.github.romainbrenguier.sedmoy.app;
 
 import com.github.romainbrenguier.sedmoy.model.DataTable;
 import com.github.romainbrenguier.sedmoy.model.FormulaTable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import org.codehaus.groovy.control.CompilationFailedException;
 
@@ -12,23 +10,21 @@ public class FormulaTableEvaluator {
   public DataTable evaluate(GroovyInterpreter interpreter, Map<String, DataTable> environment,
       FormulaTable table) {
     interpreter.setFromMap(environment);
-    final List<List<String>> lines = new ArrayList<>();
+    final DataTable result = new DataTable(table.getDimension());
     for (int line = 0; line < table.getDimension().numberOfLines; ++line) {
       interpreter.set("line", line);
-      final List<String> row = new ArrayList<>();
       for (int column = 0; column < table.getDimension().numberOfColumns; ++ column) {
         interpreter.set("column", column);
         try {
-          final Object result = interpreter.run(table.getGroovyScript());
-          row.add(result.toString());
+          final Object cellResult = interpreter.run(table.getGroovyScript());
+          result.set(column, line, cellResult.toString());
         } catch (CompilationFailedException e) {
           throw e;
         } catch (Exception e) {
-          row.add(e.toString());
+          result.set(column, line, e.toString());
         }
       }
-      lines.add(row);
     }
-    return new DataTable(lines);
+    return result;
   }
 }
