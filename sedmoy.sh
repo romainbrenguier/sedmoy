0d91f6d93ce5028e8df12e2150243cf5485748d9
Romain Brenguier <romain.brenguier@gmail.com>
2022-06-18 16:33:55 +0100

    Debug script

--- sedmoy.sh
+++ sedmoy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 DIR="$( dirname "${BASH_SOURCE[0]}" )"
-java -jar $DIR/target/sedmoy-1.0-SNAPSHOT.jar "$@"
+JVM_ARGS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
+echo $JVM_ARGS
+java $JVM_ARGS -jar $DIR/target/sedmoy-1.0-SNAPSHOT.jar "$@"
