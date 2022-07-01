b09ff8b9117540637cf375c216984c961f194825
Romain Brenguier <romain.brenguier@gmail.com>
2022-07-01 23:09:57 +0100

    Import file from gui

--- src/main/java/com/github/romainbrenguier/sedmoy/ui/GraphicalInterface.java
+++ src/main/java/com/github/romainbrenguier/sedmoy/ui/GraphicalInterface.java
@@ -2,22 +2,28 @@ package com.github.romainbrenguier.sedmoy.ui;
 
 import com.github.romainbrenguier.sedmoy.app.GroovyInterpreter;
 import com.github.romainbrenguier.sedmoy.app.TableEvaluator;
+import com.github.romainbrenguier.sedmoy.model.CsvParser;
 import com.github.romainbrenguier.sedmoy.model.DataTable;
 import com.github.romainbrenguier.sedmoy.model.Dimension;
 import com.github.romainbrenguier.sedmoy.model.Document;
 import com.github.romainbrenguier.sedmoy.model.FormulaTable;
 import com.github.romainbrenguier.sedmoy.model.Table;
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.FlowLayout;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.text.JTextComponent;
 
@@ -45,6 +51,10 @@ public class GraphicalInterface {
     toolPanel.add(saveButton);
     saveButton.addActionListener(actionEvent -> save());
 
+    final JButton importButton = new JButton("Import");
+    toolPanel.add(importButton);
+    importButton.addActionListener(actionEvent -> importCallback(frame));
+
     final JButton evaluateButton = new JButton("Evaluate");
     evaluateButton.addActionListener(actionEvent -> evaluate());
     toolPanel.add(evaluateButton);
@@ -63,6 +73,39 @@ public class GraphicalInterface {
     frame.setLocationRelativeTo(null);
     frame.pack();
     frame.setVisible(true);
+  }
+
+  private void importCallback(Component parentComponent) {
+    final JFileChooser fileChooser = new JFileChooser();
+    int dialogResult = fileChooser.showOpenDialog(parentComponent);
+    if (dialogResult == JFileChooser.APPROVE_OPTION) {
+      File file = fileChooser.getSelectedFile();
+      try {
+        Table table = importFile(file);
+        mainPanel.setVisible(false);
+        document.add(file.getName(), table);
+        graphicalComponents.initialize(mainPanel, document);
+        fillDocumentComponents(document, tableEvaluator.evaluate(document));
+        mainPanel.setVisible(true);
+      } catch (IOException exception) {
+        JOptionPane.showMessageDialog(parentComponent,
+            "File " + file + " could not be imported: " + exception,
+            "Import error",
+            JOptionPane.ERROR_MESSAGE);
+      }
+    }
+  }
+
+  private static Table importFile(File file) throws IOException {
+    final List<String> lines = Files.readAllLines(file.toPath());
+    if (file.getName().endsWith(".csv")) {
+      return new CsvParser().parseLines(lines);
+    } else if (file.getName().endsWith(".groovy")) {
+      return new FormulaTable(new Dimension(1, 1),
+          String.join("\n", lines));
+    } else {
+      throw new IOException("unknown file type " + file);
+    }
   }
 
   private void addFormula() {
