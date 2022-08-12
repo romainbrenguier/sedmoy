a5b214d4d8415af4642b61267472773d9b5468d5
Romain Brenguier <romain.brenguier@diffblue.com>
2022-08-12 10:02:20 +0100

    Handle groovy scripts in edit handler

--- plugin/src/main/java/com/github/romainbrenguier/plugin/EditHandler.java
+++ plugin/src/main/java/com/github/romainbrenguier/plugin/EditHandler.java
@@ -1,7 +1,12 @@
 package com.github.romainbrenguier.plugin;
 
+import com.github.romainbrenguier.sedmoy.app.FormulaTableEvaluator;
+import com.github.romainbrenguier.sedmoy.app.GroovyException;
+import com.github.romainbrenguier.sedmoy.app.GroovyInterpreter;
 import com.github.romainbrenguier.sedmoy.model.CsvParser;
 import com.github.romainbrenguier.sedmoy.model.DataTable;
+import com.github.romainbrenguier.sedmoy.model.Dimension;
+import com.github.romainbrenguier.sedmoy.model.FormulaTable;
 import com.github.romainbrenguier.sedmoy.ui.DataTableModel;
 import com.intellij.codeInsight.template.impl.editorActions.TypedActionHandlerBase;
 import com.intellij.openapi.actionSystem.DataContext;
@@ -15,6 +20,8 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 class EditHandler extends TypedActionHandlerBase {
@@ -34,17 +41,33 @@ class EditHandler extends TypedActionHandlerBase {
     public static void updateToolWindow(@NotNull Editor editor) {
         Document document = editor.getDocument();
         final String name = FileDocumentManager.getInstance().getFile(document).getName();
+        if (!name.endsWith(".csv") && !name.endsWith(".groovy"))
+            return;
+
+        final Project project = editor.getProject();
+        final JTable tableComponent = SedmoyToolWindowFactory.getTableComponent(project);
+        final String text = document.getText();
+        Supplier<DataTable> dataTableSupplier;
         if (name.endsWith("csv")) {
-            final Project project = editor.getProject();
-            final JTable tableComponent = SedmoyToolWindowFactory.getTableComponent(project);
-            final String text = document.getText();
-            Runnable runnable = () ->
-            {
-                final DataTable dataTable =
-                        new CsvParser().parseLines(Arrays.stream(text.split("\n")).collect(Collectors.toList()));
-                tableComponent.setModel(DataTableModel.nonEditable(dataTable));
+            dataTableSupplier = () ->
+                    new CsvParser().parseLines(Arrays.stream(text.split("\n"))
+                            .collect(Collectors.toList()));
+        } else if (name.endsWith(".groovy")) {
+            dataTableSupplier = () -> {
+                final GroovyInterpreter groovyInterpreter = new GroovyInterpreter();
+                final FormulaTable formula = new FormulaTable(new Dimension(1, 1), text);
+                try {
+                    return new FormulaTableEvaluator().evaluate(groovyInterpreter, Collections.emptyMap(), formula);
+                } catch (GroovyException e) {
+                    return new DataTable(Collections.singletonList(Collections.singletonList(e.getMessage())));
+                }
             };
-            WriteCommandAction.runWriteCommandAction(project, runnable);
+        } else {
+            throw new AssertionError("Unhandled file extension " + name);
         }
+        Runnable action = () -> {
+            tableComponent.setModel(DataTableModel.nonEditable(dataTableSupplier.get()));
+        };
+        WriteCommandAction.runWriteCommandAction(project, action);
     }
 }
