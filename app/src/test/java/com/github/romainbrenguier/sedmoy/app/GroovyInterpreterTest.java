9b05f09808bca51a90ae1327b4889f961e15a129
Romain Brenguier <romain.brenguier@gmail.com>
2022-08-21 18:14:18 +0100

    Disabled test for class path interpreter case

--- app/src/test/java/com/github/romainbrenguier/sedmoy/app/GroovyInterpreterTest.java
+++ app/src/test/java/com/github/romainbrenguier/sedmoy/app/GroovyInterpreterTest.java
@@ -1,11 +1,10 @@
 package com.github.romainbrenguier.sedmoy.app;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class GroovyInterpreterTest {
@@ -24,5 +23,27 @@ class GroovyInterpreterTest {
     // Assert
     assert(result instanceof Integer);
     assert(((Integer)result).intValue() == 10);
+  }
+
+  @Disabled
+  @Test
+  void runClasspath() throws IOException, GroovyException {
+    final Path directory = Path.of("/home/diffblue/romain"
+        + ".brenguier/git/WaterSimul/src");
+    final GroovyInterpreter interpreter = new GroovyInterpreter(
+        directory);
+
+    final Path path = directory.resolve("Main.groovy");
+
+    final String script = Files.readString(path);
+    interpreter.setScript(script);
+
+    // Act
+    final Object result = interpreter.run();
+
+    // Assert
+    assert(result instanceof Integer);
+    assert(((Integer)result).intValue() == 10);
+
   }
 }
