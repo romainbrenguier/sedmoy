3d39f66cb81e05cfd6fe883f60f199c3a18ef5d8
Romain Brenguier <romain.brenguier@diffblue.com>
2022-12-09 18:03:39 +0000

    Fix script

--- sedmoy.sh
+++ sedmoy.sh
@@ -2,8 +2,7 @@
 
 DIR="$( dirname "${BASH_SOURCE[0]}" )"
 JVM_ARGS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
-#echo $JVM_ARGS
+echo $JVM_ARGS
 #java $JVM_ARGS -jar $DIR/target/sedmoy-1.0-SNAPSHOT.jar "$@"
-cd app
 echo $@
-java -jar build/libs/app-1.0-SNAPSHOT-all.jar "$@"
+java $JVM_ARGS -jar app/build/libs/app-1.0-SNAPSHOT-all.jar "$@"
