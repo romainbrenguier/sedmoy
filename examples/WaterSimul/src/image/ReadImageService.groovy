da41642433cace20697de0d76c132b56b8840237
Romain Brenguier <romain.brenguier@diffblue.com>
2022-12-09 16:17:21 +0000

    Read image service

---
+++ examples/WaterSimul/src/image/ReadImageService.groovy
@@ -0,0 +1,17 @@
+
+package image
+
+import java.awt.image.BufferedImage
+import java.io.File
+import java.io.IOException
+import javax.imageio.ImageIO
+import java.nio.file.Paths
+
+
+def file = Paths.get(System.getProperty("user.home") + "git/sedmoy/")
+file.
+
+//def inputFile = new File(file)
+//def image = ImageIO.read(inputFile)
+//image.getRGB(0, 0)
+
