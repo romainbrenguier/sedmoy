963192ff96bfd867291913a144b50f4685478491
Romain Brenguier <romain.brenguier@diffblue.com>
2022-12-09 16:29:40 +0000

    Update sedmoy script for new source directory

--- sedmoy.sh
+++ sedmoy.sh
@@ -2,5 +2,8 @@
 
 DIR="$( dirname "${BASH_SOURCE[0]}" )"
 JVM_ARGS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
-echo $JVM_ARGS
-java $JVM_ARGS -jar $DIR/target/sedmoy-1.0-SNAPSHOT.jar "$@"
+#echo $JVM_ARGS
+#java $JVM_ARGS -jar $DIR/target/sedmoy-1.0-SNAPSHOT.jar "$@"
+cd app
+echo $@
+gradle run --args "$@"
