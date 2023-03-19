a3da8d79295f5e935e5eb901fd22d17a090e0e28
Romain Brenguier <romain.brenguier@diffblue.com>
2023-03-19 13:28:04 +0000

    Make kill and shout proper actions

--- story/src/main/java/com/github/romainbrenguier/story/Action.java
+++ story/src/main/java/com/github/romainbrenguier/story/Action.java
@@ -2,6 +2,8 @@ package com.github.romainbrenguier.story;
 
 import com.github.romainbrenguier.story.places.Room;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -9,6 +11,8 @@ import java.util.stream.Collectors;
 public interface Action {
 
     String format(Function<Integer, String> roomToString);
+
+    List<Character> actors();
 
     class Arrive implements Action {
         Character character;
@@ -22,6 +26,11 @@ public interface Action {
         @Override
         public String format(Function<Integer, String> roomToString) {
             return character + " arrives in " + roomToString.apply(inRoom) + ".";
+        }
+
+        @Override
+        public List<Character> actors() {
+            return Collections.singletonList(character);
         }
     }
 
@@ -40,6 +49,11 @@ public interface Action {
             return character + " goes from " + roomToString.apply(fromRoom) + " to " + roomToString.apply(toRoom) +
                     ".";
         }
+
+        @Override
+        public List<Character> actors() {
+            return Collections.singletonList(character);
+        }
     }
 
     class Talk implements Action {
@@ -50,14 +64,41 @@ public interface Action {
             return talking.stream().map(Character::toString)
                     .collect(Collectors.joining(", ")) + " are talking";
         }
+
+        @Override
+        public List<Character> actors() {
+            return talking;
+        }
+
+
     }
 
-    class Kill {
+    class Kill implements Action {
         Character by;
         Character target;
+
+        @Override
+        public String format(Function<Integer, String> roomToString) {
+            return by.toString() + " kills " + target.toString();
+        }
+
+        @Override
+        public List<Character> actors() {
+            return Arrays.asList(by, target);
+        }
     }
 
-    class Shout {
+    class Shout implements Action {
         Character by;
+
+        @Override
+        public String format(Function<Integer, String> roomToString) {
+            return by.toString() + " shouts";
+        }
+
+        @Override
+        public List<Character> actors() {
+            return Collections.singletonList(by);
+        }
     }
 }
